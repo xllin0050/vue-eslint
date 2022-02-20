6c23bcd91e60bf0e172ed526d213468c5c5150d2
Tanzi Carl <63276356+xllin0050@users.noreply.github.com>
2022-02-20 10:45:52 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        'vue/setup-compiler-macros': true,
+    },
+    extends: ['plugin:vue/vue3-recommended', 'airbnb-base', 'prettier'],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['vue', 'prettier'],
+    rules: {
+        'vue/script-setup-uses-vars': 'error',
+        'vue/valid-template-root': 'off',
+        'vue/no-unused-vars': 'off',
+        'vue/require-default-prop': 'off',
+        'no-console': 'off',
+        'arrow-body-style': 'off',
+        'prefer-arrow-callback': 'off',
+    },
+}
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [vue(), eslintPlugin()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 4000,
+    },
+})
